d2a6b8accfe408447a814ed1127e057f7c78993d
namittu <namittu@gmail.com>
2025-03-26 10:06:31 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,2 +1,36 @@
 alert("Hello World!")
 
+async function fetchData(url, options) {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const url =
+  "https://script.google.com/macros/s/GASのデプロイID/exec?sheetName=シート名&cell=セル名";
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+fetchData(url, options);
+
+
+// GASのurl
+const url = "https://script.google.com/macros/s/GASのデプロイID/exec";
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    sheet: "シート名",
+    cell: "セル名",
+    value: "値",
+  }),
+};
+fetchData(url, options);
