65738fa66fd16cf45f2ff2c14cce4303cc463d37
namittu <namittu@gmail.com>
2025-03-28 13:25:14 +0900

    Update tableResult.js

--- tableResult.js
+++ tableResult.js
@@ -1,12 +1,18 @@
 const data = [
+//  {
+//    "THUMBNAIL":"https://www.google.co.jp/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+//    "NAME":"Google",
+//    "URL":"https://www.google.co.jp/"},
+//  {
+//    "THUMBNAIL":"https://s.yimg.jp/images/top/sp2/cmn/logo-170307.png",
+//    "NAME":"Yahoo",
+//    "URL":"https://www.yahoo.co.jp/"},
   {
-    "THUMBNAIL":"https://www.google.co.jp/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-    "NAME":"Google",
-    "URL":"https://www.google.co.jp/"},
+    "Aチーム":"キング",
+    "Bチーム":"まーちゃん"},
   {
-    "THUMBNAIL":"https://s.yimg.jp/images/top/sp2/cmn/logo-170307.png",
-    "NAME":"Yahoo",
-    "URL":"https://www.yahoo.co.jp/"},
+    "Aチーム":"ミサちゃん",
+    "Bチーム":"鈴木さん"},
 ];
 
 TableMaker.make({tableId: 'resultTable', json: data});
